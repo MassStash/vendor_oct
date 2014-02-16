49d501d5c1fd4895443f2fa0ed8672149cab8faa
MassStash <MassStash@gmail.com>
2014-02-16 11:38:06 -0600

    Add jewel and evita to lunches menus

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -21,8 +21,8 @@ add_lunch_combo oct_jfltexx-userdebug
 #add_lunch_combo oct_m7tmo-userdebug
 #add_lunch_combo oct_m7ul-userdebug
 #add_lunch_combo oct_m7vzw-userdebug
-#add_lunch_combo oct_jewel-userdebug
-#add_lunch_combo oct_evita-userdebug
+add_lunch_combo oct_jewel-userdebug
+add_lunch_combo oct_evita-userdebug
 #add_lunch_combo oct_grouper-userdebug
 #add_lunch_combo oct_tilapia-userdebug
 #add_lunch_combo oct_hammerhead-userdebug
