6b561f191afa88734fb546725d63c669a95182f4
MassStash <MassStash@gmail.com>
2014-02-20 18:00:37 -0600

    Fix vendor and add refreshspr

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,11 +8,17 @@ add_lunch_combo oct_d2vzw-userdebug
 add_lunch_combo oct_d2mtr-userdebug
 add_lunch_combo oct_d2spi-userdebug
 add_lunch_combo oct_epicmtd-userdebug
+add_lunch_combo oct_evita-userdebug
+add_lunch_combo oct_flo-userdebug
+add_lunch_combo oct_grouper-userdebug
+add_lunch_combo oct_hammerhead-userdebug
+add_lunch_combo oct_jewel-userdebug
 add_lunch_combo oct_jflte-userdebug
 add_lunch_combo oct_jflteatt-userdebug
 add_lunch_combo oct_jfltecan-userdebug
 add_lunch_combo oct_jfltecri-userdebug
 add_lunch_combo oct_jfltecsp-userdebug
+add_lunch_combo oct_jflterefreshspr-userdebug
 add_lunch_combo oct_jfltespr-userdebug
 add_lunch_combo oct_jfltetmo-userdebug
 add_lunch_combo oct_jflteusc-userdebug
@@ -23,11 +29,6 @@ add_lunch_combo oct_jfltexx-userdebug
 #add_lunch_combo oct_m7tmo-userdebug
 #add_lunch_combo oct_m7ul-userdebug
 #add_lunch_combo oct_m7vzw-userdebug
-add_lunch_combo oct_jewel-userdebug
-add_lunch_combo oct_evita-userdebug
-add_lunch_combo oct_grouper-userdebug
-add_lunch_combo oct_tilapia-userdebug
-add_lunch_combo oct_hammerhead-userdebug
 add_lunch_combo oct_mako-userdebug
-add_lunch_combo oct_flo-userdebug
+add_lunch_combo oct_tilapia-userdebug
 
